f05c0ed9122a69620930ef04d12165ff4689cd0c
bongoman-by <bongoman@tut.by>
2021-12-30 15:29:00 +0300

    feat(app): add Router*Modules [WiP]
    *	modified:   .gitignore
    *	modified:   projects/adminpro03/src/app/app-routing.module.ts
    *	modified:   projects/adminpro03/src/app/app.module.ts
    *	new file:   projects/adminpro03/src/app/auth/auth-routing.module.ts
    *	new file:   projects/adminpro03/src/app/auth/auth.module.ts
    *	renamed:    projects/adminpro03/src/app/pages/no-page-found/no-page-found.component.html -> projects/adminpro03/src/app/no-page-found/no-page-found.component.html
    *	renamed:    projects/adminpro03/src/app/pages/no-page-found/no-page-found.component.scss -> projects/adminpro03/src/app/no-page-found/no-page-found.component.scss
    *	renamed:    projects/adminpro03/src/app/pages/no-page-found/no-page-found.component.ts -> projects/adminpro03/src/app/no-page-found/no-page-found.component.ts
    *	new file:   projects/adminpro03/src/app/pages/pages-routing.module.ts
    *	modified:   projects/adminpro03/src/app/pages/pages.component.html
    *	new file:   projects/adminpro03/src/app/pages/pages.module.ts
    *	new file:   projects/adminpro03/src/app/shared/shared.module.ts
    
    Close #4

--- projects/adminpro03/src/app/app-routing.module.ts
+++ projects/adminpro03/src/app/app-routing.module.ts
@@ -1,34 +1,22 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
-import { PagesComponent } from './pages/pages.component';
+import { PagesRoutingModule } from './pages/pages-routing.module';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    ],
-  },
-
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-
   { path: '**', component: NoPageFoundComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule,
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
