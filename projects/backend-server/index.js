cd12b5af8c7d250518f6133a559a10f37eab0849
bongoman-by <bongoman@tut.by>
2022-01-26 14:58:56 +0300

    fix index.js

--- projects/backend-server/index.js
+++ projects/backend-server/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require("path");
 
 const express = require("express");
 const cors = require("cors");
@@ -22,9 +23,22 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/hospitals", require("./routes/hospitals"));
 app.use("/api/doctors", require("./routes/doctors"));
 
-db(process.env.DB_CNN);
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "public/index.html"));
+// });
 
-app.use(express.static("projects/backend-server/public"));
+// app.use(express.static("projects/backend-server/public"));
+
+const _app_folder = __dirname + "/public";
+// ---- SERVE STATIC FILES ---- //
+app.get("*.*", express.static(_app_folder, { maxAge: "1y" }));
+
+// ---- SERVE APPLICATION PATHS ---- //
+app.all("*", function (req, res) {
+  res.status(200).sendFile(`/`, { root: _app_folder });
+});
+
+db(process.env.DB_CNN);
 
 // MongoDB admin JQdATFho1Ggy2Zms
 const port = parseInt(process.env.PORT, 10) || 8000;
