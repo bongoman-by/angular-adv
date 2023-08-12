87b618daffc04e6f3b2952d3cd0666cce16f887a
bongoman-by <s.kliuchnikoff@gmail.com>
2023-08-12 12:25:40 +0300

    fix error throw change package.json

--- projects/adminpro03/src/environments/environment.prod.ts
+++ projects/adminpro03/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  base_url: 'https://app-admin-pro.herokuapp.com/api',
+  base_url: 'https://angular-adv-be.onrender.com/api',
   usersLoadLimit: 4,
   hospitalsLoadLimit: 3,
   doctorsLoadLimit: 3,
